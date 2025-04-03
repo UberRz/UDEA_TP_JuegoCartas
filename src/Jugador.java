93de4fe8e19720fb436129cff87bcd773e11fb66
uberrz <u3er07@gmail.com>
2025-04-02 22:12:23 -0500

    Se mejora y se pone a funcionar código para detectar escaleras

--- src/Jugador.java
+++ src/Jugador.java
@@ -17,7 +17,7 @@ public class Jugador {
     public void repartir() {
         for (int i = 0; i < TOTAL_CARTAS; i++) {
             cartas[i] = new Carta(r);
-            Arrays.sort(cartas);
+
         }
 
     }
@@ -65,19 +65,26 @@ public class Jugador {
     }
 
     public String getEscaleras() {
-        int TAMAÑO_INICIAL = 15;
-        int totalDatos = 0;
+
+        int TOTAL_DATOS_TREBOL = 0;
+        int TOTAL_DATOS_PICA = 0;
+        int TOTAL_DATOS_CORAZON = 0;
+        int TOTAL_DATOS_DIAMANTE = 0;
         String mensaje = "No se encontraron Escaleras";
         int[] indiceCartas = new int[cartas.length];
-        int[] listaEscalera = new int[TAMAÑO_INICIAL];
-        int[] gruposEscalera = new int[TAMAÑO_INICIAL];
+        int[] listaTrebol = new int[10];
+        int[] listaPica = new int[10];
+        int[] listaCorazon = new int[10];
+        int[] listaDiamante = new int[10];
         int fila = 0;
 
+        // Se llena una lista con índices de las cartas repartidas
         for (Carta carta : cartas) {
             indiceCartas[fila] = carta.getIndice();
             fila++;
 
         }
+        // se ordena la lista de índices
         Jugador.ordenarIndices(indiceCartas);
         for (int j = 0; j < indiceCartas.length; j++) {
             System.out.println(indiceCartas[j]);
@@ -85,47 +92,89 @@ public class Jugador {
         }
         System.out.println("siguiente");
 
-        // si estan en escalera agregar a listaEscalera
-        for (int i = 0; i < indiceCartas.length - 1; i++) {
+        // Se separan las cartas por pinta
+        for (int i = 1; i < indiceCartas.length; i++) {
+            if (indiceCartas[i] <= 13) {
+                listaTrebol[TOTAL_DATOS_TREBOL] = indiceCartas[i];
+                TOTAL_DATOS_TREBOL++;
+            } else if (13 < indiceCartas[i] && indiceCartas[i] <= 26) {
+                listaPica[TOTAL_DATOS_PICA] = indiceCartas[i];
+                TOTAL_DATOS_PICA++;
+            } else if (26 < indiceCartas[i] && indiceCartas[i] <= 39) {
+                listaCorazon[TOTAL_DATOS_CORAZON] = indiceCartas[i];
+                TOTAL_DATOS_CORAZON++;
+            } else {
+                listaDiamante[TOTAL_DATOS_DIAMANTE] = indiceCartas[i];
+                TOTAL_DATOS_DIAMANTE++;
+            }
+        }
+        mensaje = "Se encontraron las siguientes escaleras de Trebol:\n";
+        for (int k = 0; k < listaTrebol.length - 1; k++) {
 
-            if (indiceCartas[i] + 1 == indiceCartas[i + 1]) {
-                listaEscalera[totalDatos] = indiceCartas[i - 1];
-                totalDatos++;
-                listaEscalera[totalDatos + 1] = indiceCartas[i];
+            if (listaTrebol[k] + 1 == listaTrebol[k + 1]) {
+                int indice1 = listaTrebol[k] % 13;
+                if (indice1 != 0) {
+                    mensaje += NombreCarta.values()[(listaTrebol[k] % 13) - 1] + "+"
+                            + NombreCarta.values()[(listaTrebol[k + 1] % 13) - 1] + " _";
+                } else {
+                    mensaje += NombreCarta.values()[12];
 
+                }
             }
 
         }
-        for (int j = 0; j < totalDatos; j++) {
-            System.out.println(listaEscalera[j]);
+        mensaje += "Se encontraron las siguientes escaleras de Pica:\n";
+        for (int k = 0; k < listaPica.length - 1; k++) {
+
+            if (listaPica[k] + 1 == listaPica[k + 1]) {
+                int indice1 = listaPica[k] % 13;
+                if (indice1 != 0) {
+                    mensaje += NombreCarta.values()[(listaPica[k] % 13) - 1] + "+"
+                            + NombreCarta.values()[(listaPica[k + 1] % 13) - 1] + " _";
+                } else {
+                    mensaje += NombreCarta.values()[12];
+
+                }
+            }
+
+        }
+        mensaje += "Se encontraron las siguientes escaleras de Corazon:\n";
+        for (int k = 0; k < listaCorazon.length - 1; k++) {
+
+            if (listaCorazon[k] + 1 == listaCorazon[k + 1]) {
+                int indice1 = listaCorazon[k] % 13;
+                if (indice1 != 0) {
+                    mensaje += NombreCarta.values()[(listaCorazon[k] % 13) - 1] + "+"
+                            + NombreCarta.values()[(listaCorazon[k + 1] % 13) - 1] + " _";
+                } else {
+                    mensaje += NombreCarta.values()[12];
+
+                }
+            }
+
+        }
+        mensaje += "Se encontraron las siguientes escaleras de Diamante:\n";
+        for (int k = 0; k < listaDiamante.length - 1; k++) {
+
+            if (listaDiamante[k] + 1 == listaDiamante[k + 1]) {
+                int indice1 = listaDiamante[k] % 13;
+                if (indice1 != 0) {
+                    mensaje += NombreCarta.values()[(listaDiamante[k] % 13) - 1] + "+"
+                            + NombreCarta.values()[(listaDiamante[k + 1] % 13) - 1] + " _";
+                } else {
+                    mensaje += NombreCarta.values()[12];
+
+                }
+            }
+
+        }
+        for (int i = 0; i < listaCorazon.length; i++) {
+            System.out.println(listaCorazon[i]);
 
         }
 
-        // se ordena el arreglo de indices
-
-        // para verificar las listas creadas
-
-        // boolean hayGrupos = false;
-        // for (int contador : contadores) {
-        // if (contador >= 2) {
-        // hayGrupos = true;
-        // break;
-        // }
-        // }
-
-        // if (hayGrupos) {
-        // mensaje = "Se encontraron los siguientes grupos:\n";
-        // int fila = 0;
-        // for (int contador : contadores) {
-        // if (contador >= 2) {
-        // mensaje += Grupo.values()[contador] + " de " + NombreCarta.values()[fila] +
-        // "\n";
-        // }
-        // fila++;
-        // }
-        // }
-
         return mensaje;
+
     }
 
     public int getPuntaje() {
